f2b1e69ed24ec2015f729edd46834fb0785313ad
Rohit Kumar <146173106+RoHIT4Kr7@users.noreply.github.com>
2024-12-16 18:59:21 +0530

    Infile Readme removed
    Removed in file Readme and added new dates

--- mynextapp/components/main/Accom2.jsx
+++ mynextapp/components/main/Accom2.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Swiperapp from '@/components/main/Swiper';
-import SparkleButton from './SparkleButton';
-import './Accomodation.css'
+import React, { useState } from "react";
+import Swiperapp from "@/components/main/Swiper";
+import SparkleButton from "./SparkleButton";
+import "./Accomodation.css";
 const Accom2 = () => {
   const [activeIndex, setActiveIndex] = useState(0); // Default to "Accommodation Policies"
 
@@ -11,18 +11,17 @@ const Accom2 = () => {
 
   const sections = [
     {
-      title: 'Accommodation Policies',
+      title: "Accommodation Policies",
       content: `
-   • Booking dates are from 16th–21st September
+   • Booking dates are from 18th–21st January
  • All accommodation bookings must be made through the official Splurge Tech Fest
  website.
  • Full payment is required at the time of booking.
  • Payments are non-refundable
-`
-
+`,
     },
     {
-      title: 'Contacts',
+      title: "Contacts",
       content: `
         Suman Das:\n
         Phone: +91 97498 03821 \n
@@ -30,39 +29,53 @@ const Accom2 = () => {
         Adarsh Pandey:\n
         Phone: +91 86177 58589 \n
         Email: adarshpandey2502@gmail.com
-      `
-    }
-    
+      `,
+    },
   ];
 
   return (
     <div className="flex flex-col lg:flex-row items-start lg:items-start space-y-4 lg:space-y-0 lg:space-x-10">
       <div className="w-full lg:w-1/2 lg:mt-30 lg:mt-0 lg:ml-60">
         <Swiperapp />
-        <div className='hidden lg:block Michroma mt-5'><SparkleButton text="BOOK NOW!"/></div>
+        <div className="hidden lg:block Michroma mt-5">
+          <SparkleButton text="BOOK NOW!" />
+        </div>
       </div>
       <div className="w-full lg:w-1/2 mt-10 lg:mt-10">
         {sections.map((section, index) => (
           <div key={index} className="w-full">
             <div className="w-full">
-              <div
-                className="cursor-pointer py-2 px-4 font-bold text-lg Michroma rounded-2xl"
-              >
+              <div className="cursor-pointer py-2 px-4 font-bold text-lg Michroma rounded-2xl">
                 <div className="w-full flex">
-                  <SparkleButton text={section.title} onClick={() => toggleSection(index)} />
+                  <SparkleButton
+                    text={section.title}
+                    onClick={() => toggleSection(index)}
+                  />
                 </div>
               </div>
             </div>
             {activeIndex === index && (
               <div className="content-container mt-2 py-4 px-6 border-2 border-gray-300 Michroma rounded-xl text-white mr-5 ">
-                <p className='text-red-600'>*SEPARATE HOSTEL FACILITIES FOR GIRLS AND BOYS</p>
+                <p className="text-red-600">
+                  *SEPARATE HOSTEL FACILITIES FOR GIRLS AND BOYS
+                </p>
                 <p>{section.content}</p>
-                <button className='mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'><a href='https://firebasestorage.googleapis.com/v0/b/splurge-techfest.appspot.com/o/ACCOMODATION.pdf?alt=media&token=47eda1bb-7778-4a60-b2b0-16377627de4f' target='_blank' rel="noopener noreferrer">TERMS AND CONDITIONS</a></button>
+                <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  <a
+                    href="https://firebasestorage.googleapis.com/v0/b/splurge-techfest.appspot.com/o/ACCOMODATION.pdf?alt=media&token=47eda1bb-7778-4a60-b2b0-16377627de4f"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    TERMS AND CONDITIONS
+                  </a>
+                </button>
               </div>
             )}
           </div>
         ))}
-        <button className='text-white mt-5 Michroma ml-5 lg:hidden block'>BOOK NOW!</button>
+        <button className="text-white mt-5 Michroma ml-5 lg:hidden block">
+          BOOK NOW!
+        </button>
       </div>
     </div>
   );
--- mynextapp/components/main/Accommodation.jsx
+++ mynextapp/components/main/Accommodation.jsx
@@ -1,29 +1,30 @@
 "use client";
-import React from 'react';
-import Image from 'next/image';
-import './Accomodation.css';
-import Accom2 from '@/components/main/Accom2';
-import HorizontalCard from '@/components/main/Creekcard';
+import React from "react";
+import Image from "next/image";
+import "./Accomodation.css";
+import Accom2 from "@/components/main/Accom2";
+import HorizontalCard from "@/components/main/Creekcard";
 
 const Accommodation = () => {
   return (
     <>
       <div className="promo-banner-container z-10">
         <div className="notification-bar mt-32 lg:text-xl">
-          <a 
+          <a
             href="https://the-creek-club.kolkatahotels.org/en/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            BOOK PREMIUM ROOMS AT CREEK CLUB @25% OFF, VALID ONLY TILL SPLURGE&apos;24.
+            BOOK PREMIUM ROOMS AT CREEK CLUB @25% OFF, VALID ONLY TILL
+            SPLURGE&apos;24.
           </a>
         </div>
         <Image
-          src='https://firebasestorage.googleapis.com/v0/b/splurge-techfest.appspot.com/o/bg_accom.jpg?alt=media&token=7558772a-9f45-4ac9-8043-c424e435fc69'
+          src="https://firebasestorage.googleapis.com/v0/b/splurge-techfest.appspot.com/o/bg_accom.jpg?alt=media&token=7558772a-9f45-4ac9-8043-c424e435fc69"
           alt="Background"
           className="background-image"
           fill
-          style={{ objectFit: 'cover' }}
+          style={{ objectFit: "cover" }}
         />
         <h1 className="text-3xl lg:text-6xl promo-text font-ethnocentric text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
           ACCOMMODATION
@@ -32,16 +33,16 @@ const Accommodation = () => {
 
       {/* Centering the text and controlling the border width */}
       <div className="flex justify-center mt-10">
-        <div className='font-ethnocentric inline-block text-4xl text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-700 border-b-4 border-purple-400'>
+        <div className="font-ethnocentric inline-block text-4xl text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-700 border-b-4 border-purple-400">
           OUR HOSTEL FACILITIES
         </div>
       </div>
 
-      <div className='z-10 mt-10'>
+      <div className="z-10 mt-10">
         <Accom2 />
       </div>
-      <div className='mt-5 z-10'>
-        <HorizontalCard/>
+      <div className="mt-5 z-10">
+        <HorizontalCard />
       </div>
     </>
   );
